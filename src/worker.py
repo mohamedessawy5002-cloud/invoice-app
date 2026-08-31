62ef96b366976dadf3ca6cc672c5859e425050f8
mohamedessawy5002-cloud <mohamedessawy5002@gmail.com>
2026-08-31 13:07:12 +0300

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -1,8 +1,9 @@
-from workers import wsgi
 import sys
+from pathlib import Path
 
-sys.path.append(".")
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 from main import app
+from workers import wsgi
 
 Default = wsgi.entrypoint(app)
